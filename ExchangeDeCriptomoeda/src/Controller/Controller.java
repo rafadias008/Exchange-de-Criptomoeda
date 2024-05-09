6919ad412c2cc9c0eccf700979974748ecba80d0
rafadias008 <rafaelboquira334@gmail.com>
2024-05-09 20:38:52 -0300

    Adicionado as funcionalidades da classe controller

--- ExchangeDeCriptomoeda/src/Controller/Controller.java
+++ ExchangeDeCriptomoeda/src/Controller/Controller.java
@@ -4,10 +4,62 @@
  */
 package Controller;
 
+import DAO.Conexao;
+import DAO.UsuarioDAO;
+import Model.Usuario;
+import View.Login;
+import View.PaginaUser;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author rafae
  */
 public class Controller {
     
+    private Login login;
+    
+    public Controller(Login login){
+        this.login = login;
+    }
+    
+    public void btLogin(){
+       
+        Usuario user = new Usuario(null,login.getLoginCPF().getText(),
+                              login.getLoginSenha().getText());
+        
+        Conexao conexao = new Conexao();
+        
+        try{
+            Connection conn  = conexao.getConnection();
+            System.out.println("conectou");
+            UsuarioDAO dao = new UsuarioDAO(conn);
+            System.out.println("criou dao");
+            ResultSet res = dao.consultarLogin(user);
+            System.out.println("resultado");
+            
+            if(res.next()){
+                 PaginaUser PU = new PaginaUser();
+                
+                String nome = res.getString("nome");
+                String cpf = res.getString("cpf");
+                String senha = res.getString("senha");
+                
+                
+                PU.setVisible(true);
+                
+                
+            } else {
+                JOptionPane.showMessageDialog(login, "Login não efetuado!");
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(login, "Erro de conexão!");
+        }
+    }
+        
+        
 }
