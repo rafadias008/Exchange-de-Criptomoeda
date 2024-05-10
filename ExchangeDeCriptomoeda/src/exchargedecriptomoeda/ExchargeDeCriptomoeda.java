8a854497392226f03c832bcf1ec485cf5e57f753
rafadias008 <rafaelboquira334@gmail.com>
2024-05-09 21:41:40 -0300

    chamando a janela na main

--- ExchangeDeCriptomoeda/src/exchargedecriptomoeda/ExchargeDeCriptomoeda.java
+++ ExchangeDeCriptomoeda/src/exchargedecriptomoeda/ExchargeDeCriptomoeda.java
@@ -2,19 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package exchangedecriptomoeda;
+package exchargedecriptomoeda;
+
+import View.Login;
 
 /**
  *
  * @author rafae
  */
-public class ExchangeDeCriptomoeda {
+public class ExchargeDeCriptomoeda {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Login login = new Login();
         
+        login.setVisible(true);
     }
     
 }
