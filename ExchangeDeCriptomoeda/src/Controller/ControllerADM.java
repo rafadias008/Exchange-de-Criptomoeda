d9480246075c3fd441feee7dd4982f4df3d7a1f7
Kayky Pires <Kayky Pires@ASUSSSSS.feinet.fei.edu.br>
2024-05-09 21:31:56 -0300

    arrumando controller

--- ExchangeDeCriptomoeda/src/Controller/ControllerADM.java
+++ ExchangeDeCriptomoeda/src/Controller/ControllerADM.java
@@ -4,10 +4,61 @@
  */
 package Controller;
 
+
+import DAO.AdministradorDAO;
+import Model.Administrador;
+import View.LoginADM;
+import DAO.Conexao;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+import view.PaginaADM;
 /**
  *
  * @author rafae
  */
 public class ControllerADM {
     
+    public LoginADM loginADM;
+    
+    public ControllerADM(LoginADM loginADM){
+        this.loginADM = loginADM;
+        
+    }
+    
+    public void btLoginADM(){
+        
+        Administrador userADM = new Administrador(null,loginADM.getLoginCPF().getText(),
+                              loginADM.getLoginSenha().getText());
+        
+        Conexao conexao = new Conexao();
+        
+        try{
+            Connection conn  = conexao.getConnection();
+            System.out.println("conectou");
+            AdministradorDAO dao = new AdministradorDAO(conn);
+            System.out.println("criou dao");
+            ResultSet res = dao.consultarLoginADM(userADM);
+            System.out.println("resultado");
+            
+            if(res.next()){
+                 
+                PaginaADM PA = new PaginaADM();
+                
+                String nome = res.getString("nome");
+                String cpf = res.getString("cpf");
+                String senha = res.getString("senha");
+                
+                
+                PA.setVisible(true);
+                
+                
+            } else {
+                JOptionPane.showMessageDialog(loginADM, "Login não efetuado!");
+            }
+        }catch(SQLException e){
+            JOptionPane.showMessageDialog(loginADM, "Erro de conexão!");
+        }
+    }
 }
