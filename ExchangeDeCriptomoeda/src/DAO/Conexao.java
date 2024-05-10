3c1e5f353c17a28515e3118307723a807b1d41f6
rafadias008 <rafaelboquira334@gmail.com>
2024-05-09 21:16:12 -0300

    Adicionado as funcionalidades da Conexao

--- ExchangeDeCriptomoeda/src/DAO/Conexao.java
+++ ExchangeDeCriptomoeda/src/DAO/Conexao.java
@@ -4,10 +4,19 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
 /**
  *
  * @author rafae
  */
 public class Conexao {
-    
+    public Connection getConnection()throws SQLException{
+        Connection conexao = DriverManager.getConnection(
+        "jdbc:postgresql://localhost:5432/usuario",
+        "postgres","20122960");
+        return conexao;
+    }
 }
+
