7ad732cd09720ab092951bb112383c1c53be0a2a
Kayky Pires <Kayky Pires@ASUSSSSS.feinet.fei.edu.br>
2024-05-09 21:36:13 -0300

    colocando usuario, DAO, finalizando o básico

--- ExchangeDeCriptomoeda/src/Controller/ControllerADM.java
+++ ExchangeDeCriptomoeda/src/Controller/ControllerADM.java
@@ -13,7 +13,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import javax.swing.JOptionPane;
-import view.PaginaADM;
+import View.PaginaADM;
 /**
  *
  * @author rafae
--- ExchangeDeCriptomoeda/src/Model/Usuario.java
+++ ExchangeDeCriptomoeda/src/Model/Usuario.java
@@ -10,4 +10,44 @@ package Model;
  */
 public class Usuario {
     
+    private String nome, cpf,senha;
+    
+
+    public Usuario(String nome, String cpf, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+    
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nome=" + nome + ", cpf=" + cpf + ", senha=" + senha + '}';
+    }
+    
+    
 }
