9b730d4dcaa3df3c6ff14279615bded9f9423f74
Kayky Pires <Kayky Pires@ASUSSSSS.feinet.fei.edu.br>
2024-05-09 21:30:45 -0300

    COlocando a interface

---
+++ ExchangeDeCriptomoeda/src/Model/Administrador.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author rafae
+ */
+public class Administrador {
+    private String nome = "Administrador",cpf = "00000000000",senha = "009922";
+
+    public Administrador(String nome, String cpf, String senha) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
